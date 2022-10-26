4adb089dec26ef087268591fc306a90d07e2eeba
Aleksandra <aleksandra.hinc00@gmail.com>
2022-10-26 13:15:58 +0200

    liczydlo pokazuje wszystkie wartosci

---
+++ src/Liczydlo.java
@@ -0,0 +1,8 @@
+public class Liczydlo {
+    public static void main(String[] args) {
+        int[] numbers= new int[] {2, 3, 7, 3, 3, 5, 2};
+        for (int i=0; i<numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
